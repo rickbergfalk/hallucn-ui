81dc4d1d398e0f910f87c3d94a65fa4be1fbd585
Rick Bergfalk <rick.bergfalk@gmail.com>
2026-01-08 07:01:49 -0600

    use fixed class prop

--- docs/src/heading-anchors.ts
+++ docs/src/heading-anchors.ts
@@ -17,7 +17,7 @@ function createAnchorButton(): HalButton {
   const btn = document.createElement("hal-button") as HalButton
   btn.variant = "ghost"
   btn.size = "icon-sm"
-  btn.className = "heading-anchor"
+  btn.class = "heading-anchor"
   btn.title = "Copy link to section"
   btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>`
   return btn
